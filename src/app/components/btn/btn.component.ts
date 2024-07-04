c3a6db163f151190aa659a2b7150a58d4f649f64
BilbaoJ <jessicabilbao02@gmail.com>
2024-07-04 13:49:37 -0500

    Dinamic button color

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -1,13 +1,26 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-btn',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './btn.component.html'
 })
 export class BtnComponent {
 
   @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
+  @Input() color: string = 'primary'
+
+  get colors(){
+    return {
+      'bg-primary-600': this.color === 'primary',
+      'hover:bg-primary-700': this.color === 'primary',
+      'focus:bg-primary-950': this.color === 'primary',
+      'bg-success-600': this.color === 'success',
+      'hover:bg-success-700': this.color === 'success',
+      'focus:bg-success-950': this.color === 'success',
+    };
+  }
 
 }
