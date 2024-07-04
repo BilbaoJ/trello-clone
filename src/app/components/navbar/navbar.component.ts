afbb882322011d317ccfc94b0388d8f14f8a1fb5
BilbaoJ <jessicabilbao02@gmail.com>
2024-07-04 18:37:52 -0500

    Menu design using overlay

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { BtnComponent } from "../btn/btn.component";
+import { OverlayModule } from '@angular/cdk/overlay';
 
 @Component({
     selector: 'app-navbar',
     standalone: true,
     templateUrl: './navbar.component.html',
-    imports: [RouterLink, BtnComponent]
+    imports: [RouterLink, BtnComponent, OverlayModule]
 })
 export class NavbarComponent {
 
+  isOpen : boolean = false;
 }
