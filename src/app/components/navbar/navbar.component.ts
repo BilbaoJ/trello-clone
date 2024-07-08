faa1ace4bcaceed6040ccdc65594fce029a88c2f
BilbaoJ <jessicabilbao02@gmail.com>
2024-07-08 10:19:02 -0500

    Working with Font Awesom

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,14 +2,19 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { BtnComponent } from "../btn/btn.component";
 import { OverlayModule } from '@angular/cdk/overlay';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronDown, faChevronRight, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-navbar',
     standalone: true,
     templateUrl: './navbar.component.html',
-    imports: [RouterLink, BtnComponent, OverlayModule]
+    imports: [RouterLink, BtnComponent, OverlayModule, FontAwesomeModule]
 })
 export class NavbarComponent {
 
   isOpen : boolean = false;
+  faChevronDown = faChevronDown;
+  faChevronRight = faChevronRight;
+  faArrowUpRightFromSquare = faArrowUpRightFromSquare;
 }
