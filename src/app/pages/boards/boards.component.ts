e0dc193b00e42e05eca1e01571c48aa725395e0c
BilbaoJ <jessicabilbao02@gmail.com>
2024-07-08 14:43:22 -0500

    Boards page design

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTrello } from '@fortawesome/free-brands-svg-icons';
+import { faWaveSquare, faChevronDown, faClock, faUser, faGear, faBriefcase, faInfo} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FontAwesomeModule],
   templateUrl: './boards.component.html'
 })
 export class BoardsComponent {
 
+  faTrello = faTrello;
+  faWaveSquare = faWaveSquare;
+  faChevronDown = faChevronDown;
+  faClock = faClock;
+  faUser = faUser;
+  faGear = faGear;
+  faBriefcase = faBriefcase;
+  faInfo = faInfo;
 }
