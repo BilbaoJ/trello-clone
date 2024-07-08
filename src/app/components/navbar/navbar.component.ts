6ac24c06bab3126d764fc243f766780c3f31b3ec
BilbaoJ <jessicabilbao02@gmail.com>
2024-07-08 16:53:36 -0500

    Dinamic navbar colors

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,20 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { BtnComponent } from "../btn/btn.component";
 import { OverlayModule } from '@angular/cdk/overlay';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faChevronDown, faChevronRight, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faChevronRight, faArrowUpRightFromSquare, faBell, faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-navbar',
     standalone: true,
     templateUrl: './navbar.component.html',
-    imports: [RouterLink, BtnComponent, OverlayModule, FontAwesomeModule]
+    imports: [CommonModule, RouterLink, BtnComponent, OverlayModule, FontAwesomeModule]
 })
 export class NavbarComponent {
+
+  @Input() textColor: string = 'gray';
+  @Input() bgColor: string = 'white';
+  @Input() logoColor: string = 'neutral';
 
   isOpen : boolean = false;
   faChevronDown = faChevronDown;
   faChevronRight = faChevronRight;
   faArrowUpRightFromSquare = faArrowUpRightFromSquare;
+  faBell = faBell;
+  faCircleQuestion = faCircleQuestion;
+
+  get colorText(){
+    return {
+      'text-gray-600': this.textColor === 'gray',
+      'text-white': this.textColor === 'white',
+    };
+  }
+
+  get bgColors(){
+    return {
+      'bg-white': this.bgColor === 'white',
+      'bg-primary-600': this.bgColor === 'blue',
+    };
+  }
+
+  get hover(){
+    return {
+      'hover:bg-gray-300': this.bgColor === 'white',
+      'hover:bg-primary-500': this.bgColor === 'blue',
+    }
+  }
+
+  get logo(){
+    return `/assets/logo/images/logo-gradient-${this.logoColor}-trello.png`
+  }
 }
