1b01e7d689e48320e1fef268bdd847ae607812d9
BilbaoJ <jessicabilbao02@gmail.com>
2024-07-08 15:12:40 -0500

    Menu design using accordion

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
-import { faWaveSquare, faChevronDown, faClock, faUser, faGear, faBriefcase, faInfo} from '@fortawesome/free-solid-svg-icons';
+import { faWaveSquare, faChevronDown, faChevronUp, faClock, faUser,
+          faGear, faBriefcase, faInfo, faHeart} from '@fortawesome/free-solid-svg-icons';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent, FontAwesomeModule],
+  imports: [NavbarComponent, FontAwesomeModule, CdkAccordionModule],
   templateUrl: './boards.component.html'
 })
 export class BoardsComponent {
@@ -15,9 +17,11 @@ export class BoardsComponent {
   faTrello = faTrello;
   faWaveSquare = faWaveSquare;
   faChevronDown = faChevronDown;
+  faChevronUp = faChevronUp;
   faClock = faClock;
   faUser = faUser;
   faGear = faGear;
   faBriefcase = faBriefcase;
   faInfo = faInfo;
+  faHeart = faHeart;
 }
