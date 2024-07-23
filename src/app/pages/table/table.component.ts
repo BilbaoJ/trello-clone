87025ca23d7751211feebf9952d5038beb18e35c
BilbaoJ <jessicabilbao02@gmail.com>
2024-07-23 15:54:53 -0500

    Table styles

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -3,16 +3,18 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 import {CdkTableModule} from '@angular/cdk/table';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { Product } from '../../models/product.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [ NavbarComponent, CdkTableModule, HttpClientModule ],
+  imports: [ NavbarComponent, CdkTableModule, HttpClientModule, CommonModule ],
   templateUrl: './table.component.html'
 })
 export class TableComponent {
   products = signal<Product[]>([]);
   columns = signal(['id', 'title', 'price']);
+  total = signal(0);
 
   constructor(
     private http: HttpClient
@@ -22,6 +24,7 @@ export class TableComponent {
     this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products').subscribe({
       next: (products)=>{
         this.products.set(products);
+        this.total.set(products.map(item => item.price).reduce((price, total) => price + total, 0));
       },
       error: () => {
 
