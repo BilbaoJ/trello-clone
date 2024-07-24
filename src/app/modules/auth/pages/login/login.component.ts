2adbf99701832d9d1fce6aa0767cda3fbd4162de
BilbaoJ <jessicabilbao02@gmail.com>
2024-07-24 16:21:56 -0500

    Home page components

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { BackgroundComponent } from '@auth/components/background/background.component';
+import { FooterComponent } from '@auth/components/footer/footer.component';
+import { HeaderComponent } from '@auth/components/header/header.component';
 import { BtnComponent } from '@shared/components/btn/btn.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [BtnComponent],
+  imports: [BtnComponent, BackgroundComponent, FooterComponent, HeaderComponent],
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
