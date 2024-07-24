8b57598e05095cdac660498b8ec15fbbf5a71138
BilbaoJ <jessicabilbao02@gmail.com>
2024-07-24 17:09:52 -0500

    Forgot password page design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from '@auth/pages/login/login.component';
 import { BoardsComponent } from '@boards/pages/boards/boards.component';
 import { BoardComponent } from '@boards/pages/board/board.component';
+import { ForgotPasswordComponent } from '@auth/pages/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
   {
@@ -16,4 +17,8 @@ export const routes: Routes = [
     path:'board',
     component: BoardComponent
   },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent
+  }
 ];
--- src/app/modules/auth/pages/forgot-password/forgot-password.component.ts
+++ src/app/modules/auth/pages/forgot-password/forgot-password.component.ts
@@ -6,16 +6,16 @@ import { HeaderComponent } from '@auth/components/header/header.component';
 import { BtnComponent } from '@shared/components/btn/btn.component';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-forgot-password',
   standalone: true,
-  imports: [BtnComponent,
-            BackgroundComponent,
+  imports: [HeaderComponent,
             FooterComponent,
-            HeaderComponent,
+            BackgroundComponent,
+            BtnComponent,
             RouterLink
           ],
-  templateUrl: './login.component.html'
+  templateUrl: './forgot-password.component.html'
 })
-export class LoginComponent {
+export class ForgotPasswordComponent {
 
 }
