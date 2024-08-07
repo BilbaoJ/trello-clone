ad712762870f9eea422c0e9d937736ecd11f09d7
BilbaoJ <jessicabilbao02@gmail.com>
2024-08-07 16:30:40 -0500

    Password recovery flow

--- src/app/modules/auth/pages/forgot-password/forgot-password.component.ts
+++ src/app/modules/auth/pages/forgot-password/forgot-password.component.ts
@@ -1,21 +1,58 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { BackgroundComponent } from '@auth/components/background/background.component';
 import { FooterComponent } from '@auth/components/footer/footer.component';
 import { HeaderComponent } from '@auth/components/header/header.component';
+import { AuthService } from '@services/auth.service';
 import { BtnComponent } from '@shared/components/btn/btn.component';
+import { RequestStatus } from '@shared/models/request-status.model';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [HeaderComponent,
+  imports: [CommonModule,
+            HeaderComponent,
             FooterComponent,
             BackgroundComponent,
             BtnComponent,
-            RouterLink
+            RouterLink,
+            ReactiveFormsModule
           ],
   templateUrl: './forgot-password.component.html'
 })
 export class ForgotPasswordComponent {
+
+  private authService = inject(AuthService);
+  form = new FormGroup({
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.email
+      ]
+    })
+  });
+  status = signal<RequestStatus>('init');
+  emailSent = signal(false);
+
+  sendLink(){
+    if (this.form.valid) {
+      this.status.set('loading');
+      const {email} = this.form.getRawValue();
+      this.authService.recovery(email).subscribe({
+        next: () => {
+          this.status.set('success');
+          this.emailSent.set(true);
+        },
+        error: (error) => {
+          this.status.set('failed');
+        }
+      });
+    }else{
+      this.form.markAllAsTouched();
+    }
+  }
 
 }
--- src/app/modules/auth/pages/recovery/recovery.component.ts
+++ src/app/modules/auth/pages/recovery/recovery.component.ts
@@ -1,11 +1,15 @@
-import { Component, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { BackgroundComponent } from '@auth/components/background/background.component';
 import { FooterComponent } from '@auth/components/footer/footer.component';
 import { HeaderComponent } from '@auth/components/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BtnComponent } from '@shared/components/btn/btn.component';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '@services/auth.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CustomValidators } from '@utils/validators';
+import { RequestStatus } from '@shared/models/request-status.model';
 
 @Component({
   selector: 'app-recovery',
@@ -15,13 +19,67 @@ import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
             BackgroundComponent,
             BtnComponent,
             RouterLink,
-            FontAwesomeModule
+            FontAwesomeModule,
+            ReactiveFormsModule
           ],
   templateUrl: './recovery.component.html'
 })
 export class RecoveryComponent {
 
+  private authService = inject(AuthService);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  form = new FormGroup({
+    newPassword: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(8)
+      ]
+    }),
+    confirmPassword: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required
+      ]
+    })
+  },
+  {
+    validators: [ CustomValidators.MatchValidator('password', 'confirmPassword') ]
+  });
+  status = signal<RequestStatus>('init');
+  token = signal('');
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = signal(false);
+
+  constructor(){
+    this.route.queryParamMap.subscribe(params => {
+      const token = params.get('token');
+      if (token) {
+        this.token.set(token);
+      }else{
+        this.router.navigate(['login']);
+      }
+    })
+  }
+
+  recovery(){
+    if (this.form.valid) {
+      const { newPassword } = this.form.getRawValue();
+      this.status.set('loading');
+      this.authService.changePassword(this.token(), newPassword)
+      .subscribe({
+        next: () => {
+          this.status.set('success');
+          this.router.navigate(['login']);
+        },
+        error: () => {
+          this.status.set('failed');
+        }
+      })
+    }else{
+      this.form.markAllAsTouched();
+    }
+  }
 }
