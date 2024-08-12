8197f9584ab8626b4a5bf94edd2d915fdb573d7b
BilbaoJ <jessicabilbao02@gmail.com>
2024-08-12 18:00:51 -0500

    Getting boards

--- src/app/modules/boards/pages/boards/boards.component.ts
+++ src/app/modules/boards/pages/boards/boards.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NavbarComponent } from '@shared/components/navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
@@ -6,6 +6,8 @@ import { faWaveSquare, faChevronDown, faChevronUp, faClock, faUser,
           faGear, faBriefcase, faInfo, faHeart} from '@fortawesome/free-solid-svg-icons';
 import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { RouterLink } from '@angular/router';
+import { MeService } from '@services/me.service';
+import { Board } from '@shared/models/board.model';
 
 @Component({
   selector: 'app-boards',
@@ -15,6 +17,8 @@ import { RouterLink } from '@angular/router';
 })
 export default class BoardsComponent {
 
+  private meService = inject(MeService);
+  boards: Board[] = [];
   faTrello = faTrello;
   faWaveSquare = faWaveSquare;
   faChevronDown = faChevronDown;
@@ -25,4 +29,15 @@ export default class BoardsComponent {
   faBriefcase = faBriefcase;
   faInfo = faInfo;
   faHeart = faHeart;
+
+  ngOnInit(){
+    this.getMeBoards();
+  }
+
+  getMeBoards(){
+    this.meService.getBoards()
+    .subscribe(boards => {
+      this.boards = boards;
+    });
+  }
 }
--- src/app/services/me.service.ts
+++ src/app/services/me.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import { User } from '@shared/models/user.model';
 import { checkToken } from '@interceptors/token.interceptor';
+import { Board } from '@shared/models/board.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +14,14 @@ export class MeService {
   apiUrl = environment.API_URL;
 
   getProfile() {
-    return this.http.get<User>(`${this.apiUrl}/api/v1/me/profile`, { context: checkToken() })
+    return this.http.get<User>(`${this.apiUrl}/api/v1/me/profile`,{
+      context: checkToken()
+    });
   }
 
   getBoards() {
-    return this.http.get<User>(`${this.apiUrl}/api/v1/me/boards`, { context: checkToken() })
+    return this.http.get<Board[]>(`${this.apiUrl}/api/v1/me/boards`, {
+      context: checkToken()
+    });
   }
 }
