f83fef8bf9bd2f2bb1d92fbf3477cb5fe6d036e6
BilbaoJ <jessicabilbao02@gmail.com>
2024-08-13 17:50:40 -0500

    Button component adjustment

--- src/app/modules/shared/components/btn/btn.component.ts
+++ src/app/modules/shared/components/btn/btn.component.ts
@@ -1,17 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { COLORS, Colors } from '@shared/models/colors.model';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-btn',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FaIconComponent],
   templateUrl: './btn.component.html'
 })
 export class BtnComponent {
 
+  @Input() disabled = false;
+  @Input() loading = false;
   @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
   @Input() color: Colors = 'primary';
+  faSpinner = faSpinner;
 
   mapColors = COLORS;
 
